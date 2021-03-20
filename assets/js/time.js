5ff33408f6b54de8d5412a9ec5167d6b41e98291
reidycolm <reidycolm@gmail.com>
2021-03-20 13:24:16 +0000

    added basic countdown timer in time.js when play icon is clicked

--- assets/js/time.js
+++ assets/js/time.js
@@ -1,1 +1,24 @@
-console.log("test");
+var minutes;
+var seconds;
+
+function startTimer() {
+    minutes = 25;
+    seconds = 0;
+
+var interval = setInterval(
+    function() {
+        var timer = document.getElementById("timer");
+        if (seconds == 0) {
+            minutes = minutes - 1;
+            seconds = 59;
+            timer.innerHTML = minutes + ":" + seconds;
+        } else if (seconds > 0) {
+            seconds = seconds - 1;
+            timer.innerHTML = minutes + ":" + seconds;
+        }
+
+
+    }, 1000
+)
+
+} 
