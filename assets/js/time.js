3b75bb6af5384bcc4f2193aba7b2457a1c8dc97f
reidycolm <reidycolm@gmail.com>
2021-03-24 14:47:33 +0000

    replaced click class on control buttons in html with click event in js file

--- assets/js/time.js
+++ assets/js/time.js
@@ -54,7 +54,7 @@ function startTimer() {
             }
         }
 
-    }, 10);
+    }, 1000);
 }
 
 function pauseTimer() {
@@ -144,4 +144,10 @@ $(document).ready(function(){
                 $("#timer, .icons, #work-duration, #break-duration").css("color", "snow");
             }
         });
+
+        $("#play").click(startTimer);
+
+        $("#pause").click(pauseTimer);
+
+        $("#reset").click(resetTimer);
 });
